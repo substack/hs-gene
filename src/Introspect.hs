15e67c900644687f8d6d66c241359fc8157dbe34
James Halliday <substack@gmail.com>
2010-04-01 01:15:18 -0800

    preprocessing and headway on loading from a string

--- src/Introspect.hs
+++ src/Introspect.hs
@@ -4,41 +4,42 @@ import Language.Haskell.Parser
 import Language.Haskell.Syntax
 import Language.Haskell.Pretty
 import Language.Haskell.Interpreter
+import qualified Language.Preprocessor.Cpphs as C
 
 import Control.Applicative ((<$>),(<*>))
 import Control.Arrow ((&&&))
-import Control.Monad (mzero,msum)
 import Control.Monad.CatchIO
 
 import Data.Maybe (mapMaybe)
 import qualified Data.Typeable as T
-import qualified Data.Typeable
-import Data.List (intersperse)
-import qualified Data.Map as M
 
-main = do
-    m <- parseModule <$> readFile "src/Introspect.hs"
-    let ParseOk (HsModule srcLoc mod mExports imports decls) = m
-        f (HsPatBind _ (HsPVar (HsIdent ident)) rhs _) =
-            Just (ident, prettyPrint rhs)
-        f _ = Nothing
-        identMap = M.fromList $ mapMaybe f decls
-    print $ identMap
+import System.Directory (copyFile)
+import System.Random
 
-functions :: (MonadCatchIO m, Functor m) =>
-      String -> m (Either InterpreterError [(String, T.TypeRep)])
-functions src = runInterpreter $ do
-    loadModules [src]
+type Defs = [(String,String)]
+
+functions :: (MonadCatchIO m, Functor m)
+    => FilePath -> Defs
+    -> m (Either InterpreterError [(String, T.TypeRep)])
+functions srcFile defs = runInterpreter $ do
+    loadModuleFromString srcFile defs
+    
     ModuleInfo {
         moduleImports = imports,
         moduleName = name
-    } <- liftIO (info src)
+    } <- liftIO (info srcFile defs)
     
-    setImportsQ $ (name,Nothing) : ("Data.Typeable",Nothing) : imports
+    setImportsQ
+        $ (name,Nothing)
+        : ("Data.Typeable",Nothing)
+        : imports
     
     mapM g =<< mapMaybe f <$> getModuleExports name
         where
-            typeRepExpr x = "Data.Typeable.typeOf (undefined :: " ++ x ++ ")"
+            typeRepExpr x =
+                "Data.Typeable.typeOf (undefined :: "
+                    ++ x
+                ++ ")"
             interp x = interpret (typeRepExpr x) (as :: T.TypeRep)
             g x = ((,) x) <$> (interp =<< typeOf x)
             f (Fun x) = Just x
@@ -49,11 +50,11 @@ type ImportQ = (ModuleName,Maybe ModuleName)
 data ModuleInfo = ModuleInfo {
     moduleImports :: [ImportQ],
     moduleName :: ModuleName
-}
+} deriving Show
 
-info :: String -> IO ModuleInfo
-info src = do
-    m <- parseModule <$> readFile src
+info :: FilePath -> Defs -> IO ModuleInfo
+info srcFile defs = do
+    m <- parseModule <$> preprocess srcFile defs
     let ParseOk (HsModule srcLoc (Module modName) mExports imports decls) = m
         f = mName . importModule &&& (Just mName <*>) . importAs
         mName (Module name) = name
@@ -65,3 +66,14 @@ info src = do
         moduleImports = ims',
         moduleName = modName
     }
+
+preprocess :: FilePath -> Defs -> IO String
+preprocess srcFile defs =
+    C.macroPass defs opts
+    =<< C.cppIfdef srcFile defs [] opts
+    =<< readFile srcFile
+    where opts = C.defaultBoolOptions { C.locations = False }
+
+loadModuleFromString :: MonadInterpreter m => FilePath -> Defs -> m ()
+loadModuleFromString srcFile defs =
+    loadModules [srcFile]
