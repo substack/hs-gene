2c2c6088d678476bb71f71e0d5157589cf85f601
James Halliday <substack@gmail.com>
2010-05-06 03:08:12 -0800

    codebase simplified: got rid of ModuleInfo and getInfo

--- src/Pointfree.hs
+++ src/Pointfree.hs
@@ -12,46 +12,35 @@ import qualified Language.Haskell.Pointfree.Common as PF
 import qualified Language.Haskell.Pointfree.Parser as PF
 
 import Control.Applicative ((<$>),(<*>))
-import Control.Arrow ((&&&),second)
-import Control.Monad (liftM2,join)
 import Control.Monad.CatchIO
 import Control.Monad.Trans (liftIO)
 import System.Random (randomRIO,randomRs,newStdGen)
 
 import qualified Data.Map as M
-import Data.List ((\\))
 
 import Data.Generics (Data(..),Typeable(..),everywhereM,mkM)
 
 type Export = (String,String)
+type Pool = [Export]
 
 type Interpreter a = (MonadCatchIO m, Functor m) => m (Either H.InterpreterError a)
 type InterpreterT a = (MonadCatchIO m, Functor m) => H.InterpreterT m a
 
 type ImportQ = (H.ModuleName,Maybe H.ModuleName)
 
-data ModuleInfo = ModuleInfo {
-    moduleImports :: [ImportQ],
-    moduleExports :: [Export],
-    moduleName :: H.ModuleName
-} deriving Show
+withModule :: FilePath -> [ImportQ] -> (String -> InterpreterT a) -> IO a
+withModule srcFile imports f = do
+    name <- getName srcFile
+    r <- H.runInterpreter $ do
+        H.loadModules [srcFile]
+        H.setImportsQ $ (name,Nothing) : imports
+        f name
+    case r of
+        Left msg -> fail $ show msg
+        Right x -> return x
 
-withModule :: FilePath -> [ImportQ]
-    -> (ModuleInfo -> InterpreterT a) -> Interpreter a
-withModule srcFile imports f = H.runInterpreter $ do
-    H.loadModules [srcFile]
-    
-    mInfo@ModuleInfo {
-        moduleImports = mImports,
-        moduleName = name
-    } <- liftIO (getInfo srcFile)
-    
-    H.setImportsQ $ (name,Nothing) : imports ++ mImports
-    exps <- exports name
-    f $ mInfo { moduleExports = exps }
-
-exports :: H.Id -> InterpreterT [(String,String)]
-exports name = mapM f =<< (concatMap ids <$> H.getModuleExports name)
+getExports :: H.Id -> InterpreterT [(String,String)]
+getExports name = mapM f =<< (concatMap ids <$> H.getModuleExports name)
     where
         f :: String -> InterpreterT (String,String)
         f x = ((,) x) <$> H.typeOf x
@@ -61,8 +50,8 @@ exports name = mapM f =<< (concatMap ids <$> H.getModuleExports name)
         ids (H.Class _ xs) = xs
         ids (H.Data _ xs) = xs
 
-getInfo :: FilePath -> IO ModuleInfo
-getInfo srcFile = do
+getName :: FilePath -> IO String
+getName srcFile = do
     src <- readFile srcFile
     ($ LH.parseModule src) $ \m -> case m of
         LH.ParseFailed loc msg ->
@@ -71,34 +60,22 @@ getInfo srcFile = do
                 ++ msg
                 ++ '\n' : (lines src !! (LH.srcLine loc - 1))
                 ++ '\n' : replicate (LH.srcColumn loc - 1) ' ' ++ "^-- here\n"
-        LH.ParseOk (LH.HsModule srcLoc (LH.Module modName) mExports imports decls) -> do
-            return $ ModuleInfo {
-                    moduleImports = ims',
-                    moduleName = modName,
-                    moduleExports = undefined
-                }
-                where
-                    f = mName . LH.importModule &&& (Just mName <*>) . LH.importAs
-                    mName (LH.Module name) = name
-                    ims = map f imports
-                    ims' = if elem "Prelude" $ map fst ims
-                        then ims
-                        else ("Prelude",Nothing) : ims
+        LH.ParseOk (LH.HsModule _ (LH.Module name) _ _ _) -> return name
 
 unpoint :: String -> PF.Expr
 unpoint = (\(PF.TLE x) -> x) . (\(Right e) -> e) . pointfree
 
 -- order-1 mutations
 mutate :: FilePath -> String -> IO String
-mutate srcFile expr = 
-    (show <$>) . withModule srcFile imports $ \info -> everyExp (unpoint expr)
-        $ \e -> do
-            t <- H.typeOf $ show e
-            let matches = show e : [ name | (name,eType) <- moduleExports info,
-                    eType == t, name /= show e ]
-            i <- liftIO $ randomRIO (0, length matches - 1)
-            liftIO $ print (i,t,matches)
-            return $ unpoint $ matches !! i
+mutate srcFile expr = (show <$>) . withModule srcFile imports $ \name -> do
+    exports <- getExports name
+    everyExp (unpoint expr) $ \e -> do
+        t <- H.typeOf $ show e
+        let matches = show e : [ name | (name,eType) <- exports,
+                eType == t, name /= show e ]
+        i <- liftIO $ randomRIO (0, length matches - 1)
+        liftIO $ print (i,t,matches)
+        return $ unpoint $ matches !! i
     where
         imports = [("Control.Monad",Nothing),("Control.Arrow",Nothing)]
         
@@ -143,9 +120,3 @@ parseTypeSig expr = sigWalk xType where
 subTypes :: TypeSig -> [TypeSig]
 subTypes t@(TypeFun t1 t2) = t : subTypes t1 ++ subTypes t2
 subTypes t = [t]
-
--- Note: doesn't work with multi-param type classes
-instanceOf :: String -> String -> InterpreterT Bool
-instanceOf xType xInstance = H.typeChecks $
-    "let f :: (" ++ xInstance ++ " a) => "
-    ++ "a -> a; f = id in f (undefined :: " ++ xType ++ ")"
